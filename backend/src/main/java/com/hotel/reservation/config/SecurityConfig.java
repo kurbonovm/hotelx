bfabe9f8354ec1ecd7ca2e341059478c852c00b9
Muhiddin Kurbonov <muhiddindc@gmail.com>
2025-12-18 19:51:23 -0600

    update .yml

--- backend/src/main/java/com/hotel/reservation/config/SecurityConfig.java
+++ backend/src/main/java/com/hotel/reservation/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.hotel.reservation.security.oauth2.CustomOAuth2UserService;
 import com.hotel.reservation.security.oauth2.OAuth2AuthenticationSuccessHandler;
 import com.hotel.reservation.security.oauth2.OAuth2AuthenticationFailureHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
